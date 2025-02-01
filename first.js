10e5039670e32b19e84ecbb3adba237915f64339
MuzammilKittur <Muzammilkhan732@gmail.com>
2025-02-01 21:52:50 +0530

    variables

--- first.js
+++ first.js
@@ -1,2 +1,13 @@
 console.log("Hello");
 console.log('JS');
+
+// above is the console to print in console page of browser.
+
+
+// Variables can be Declared in JS using "var", "let", "const"
+// after 2015 we got ES6 from JS, Where we can use var, let & const?
+
+// Ans :- "var" : we shouldnot use var bcz this will allow to declare same variables multiple times, which may lead to errors in code.
+// have practice to use always "let" & "const".
+// bcz "let" variable will not allow to declare same variables multiple times, and it allows to change the values.
+// "const" will not allow to change value of a variable.
