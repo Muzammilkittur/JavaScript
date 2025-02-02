c915a9941a289c26a0b500e735ad67055ff41596
MuzammilKittur <Muzammilkhan732@gmail.com>
2025-02-02 14:42:37 +0530

    for Loops

---
+++ _3_Loops.js
@@ -0,0 +1,17 @@
+// we have 3 types of Loops in JS,
+// For Loops,
+// While Loops,
+// do while Loops.
+
+// 1- For Loops
+
+for (let i=1; i<= 5; i++) {
+    console.log("Muzammil")
+}
+
+// sum of loop.
+let sum = 0;
+for (let i=1; i<=5; i++){
+    sum = sum+ i;
+}
+console.log("sum of i is,", sum)
