639159e02bb613d218776b036e61d21fabd07030
MuzammilKittur <Muzammilkhan732@gmail.com>
2025-02-03 16:35:33 +0530

    string interpolation

--- _3_Loops.js
+++ _3_Loops.js
@@ -21,3 +21,14 @@ console.log("sum of i is,", sum)
 for (i=0; i<10; i++) {
     console.log("print value of i =", i)
 }
+
+
+// Template Strings.
+// Template Strings are the special Strings, created by the `Back tick`.. and these are used for "String Interpolation"
+
+//"String Interpolation" means in a Template String we can add an Expression or variable using "${variable}"
+
+
+let specialString = `the result of Sum is ${sum}`;
+console.log(specialString)
+
