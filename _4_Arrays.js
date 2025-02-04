66c7d4c573be94ad2f42840e3a5e8af53e8ca624
MuzammilKittur <Muzammilkhan732@gmail.com>
2025-02-05 00:41:02 +0530

    splice method

--- _4_Arrays.js
+++ _4_Arrays.js
@@ -62,5 +62,23 @@ console.log(mark)
 mark.pop();
 console.log(mark)
 
-markString = mark.toString();
+let markString = mark.toString();
 console.log(markString)
+
+let sliceMethode = mark.slice(2, 4)
+console.log(sliceMethode)
+
+
+// splice method, Splice(): it changes original array (add, remove, replace)
+// splice(startIndex, delCount, newElement1....)
+
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log('num array is', num)
+let spliceMethode = num.splice(2, 3);   // start index is 2 and from start index it is deleting upto index 3. 
+console.log(spliceMethode)  // ans is [3, 4, 5]
+
+
+let num1 = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+console.log('num1 array is', num1)
+let spliceMethode1 = num1.splice(2, 3, 101, 102);   // array added 101, 102 from index 2
+console.log(num1)  // Modified array will be stored in num1, not in spliceMethode1
