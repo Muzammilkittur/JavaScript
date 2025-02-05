282fefac4a155e18c32ffc40e4d90c28b66030b6
MuzammilKittur <Muzammilkhan732@gmail.com>
2025-02-05 21:12:23 +0530

    function

---
+++ _5_functions.js
@@ -0,0 +1,7 @@
+// Functions in JS.
+
+// myFunction is a function we created, and a function is written as 'console.log("print function");'
+function myFunction() {
+    console.log("print function");  // but this will not execute till the function is called.
+}
+myFunction()   // here myFunction is called, so result will print in console.
