293a5d09d40242672ad419f99a5db0341dcc4344
MuzammilKittur <Muzammilkhan732@gmail.com>
2025-02-12 22:58:10 +0530

    none

--- _5_functions.js
+++ _5_functions.js
@@ -5,3 +5,4 @@ function myFunction() {
     console.log("print function");  // but this will not execute till the function is called.
 }
 myFunction()   // here myFunction is called, so result will print in console.
+//...
